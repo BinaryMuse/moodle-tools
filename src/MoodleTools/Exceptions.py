835fc6aabbecf8d295ecd4fc949b00060f656352
Brandon Tilley <brandon.tilley@fresno.edu>
2010-02-25 13:34:47 -0800

    Modified MoodleTools.Exceptions __init()__ and __str()__ to allow easier access to details.

--- src/MoodleTools/Exceptions.py
+++ src/MoodleTools/Exceptions.py
@@ -4,10 +4,19 @@ class TooManyArgsError(Exception):
     pass
 
 class ServerNotFoundError(Exception):
-    pass
+    def __init__(self, server):
+        self.server = server
+    def __str__(self):
+        return self.server
     
 class AreaNotFoundError(Exception):
-    pass
+    def __init__(self, area):
+        self.area = area
+    def __str__(self):
+        return self.area
     
 class InvalidDataKeyError(Exception):
-    pass
+    def __init__(self, key):
+        self.key = key
+    def __str__(self):
+        return self.key
