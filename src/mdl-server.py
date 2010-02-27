189938be13d4e120dea3e73cb0786308f1e15531
Brandon Tilley <brandon.tilley@fresno.edu>
2010-02-26 16:51:01 -0800

    Refactored mdl-server code somewhat:
     * moved option parsing out of MdlServer class and into calling script
     * cleaned up comments, etc
     * updated unit tests appropriately

--- src/mdl-server.py
+++ src/mdl-server.py
@@ -1,16 +1,28 @@
 #!/usr/bin/env python
 
+import sys
+from optparse import OptionParser
 from MoodleTools.MdlServer import MdlServer
 from MoodleTools.Exceptions import *
-import sys
 
 err = sys.stderr
+
+# ==============================================================================
+# Parse options
+# ==============================================================================
+
+parser = OptionParser(usage="%prog [options] [<server> [<area> [<data>]]]",
+    description="Outputs configuration data. Specify a server, a server and an area, or a server, " +
+    "area, and data element (src or data) to print appropriate data.", version="Moodle Tools mdl-server 0.1")
+parser.add_option("-f", "--file", metavar="FILE", help="use FILE instead of mdl-server.xml", default="mdl_server.xml")
+
+(options, args) = parser.parse_args()
 
 # ==============================================================================
 # Execute the program
 # ==============================================================================
 try:
-    mdl_svr = MdlServer()
+    mdl_svr = MdlServer(options, args)
     mdl_svr.go()
 except IOError as (errorno, errorstr):
     print >> err, "{0} was not found. See the --file option if you want to parse another XML file.".format(errorstr)
--- test/test_MdlServer.py
+++ test/test_MdlServer.py
@@ -1,24 +1,31 @@
 #!/usr/bin/env python
 
-import unittest
-from MoodleTools.MdlServer import MdlServer
 import os
+import unittest
+from optparse import Values
+from MoodleTools.MdlServer import MdlServer
 
 class test_MdlServer(unittest.TestCase):
     def setUp(self):
-        """Set up some default arguments."""
-        self.mdlsrv = MdlServer()
-        self.mdlsrv.parseOptions()
-        self.mdlsrv.options.file = 'test/test_mdl-server.xml'
-        self.mdlsrv.args = []
+        '''
+        Set up some default arguments.
+        '''
+        options = Values()
+        options.file = 'test/test_mdl-server.xml'
+        args = []
+        self.mdlsrv = MdlServer(options, args)
         
     def testFileNotFound(self):
-        """Test that an incorrect or missing XML file generates an exception."""
+        '''
+        Test that an incorrect or missing XML file generates an exception.
+        '''
         self.mdlsrv.options.file = "notfound.xml"
         self.assertRaises(IOError, self.mdlsrv.parse)
     
     def testCorrectServers(self):
-        """Test that the class returns the correct servers from the XML file."""
+        '''
+        Test that the class returns the correct servers from the XML file.
+        '''
         self.mdlsrv.parse()
         servers = self.mdlsrv.servernodes.keys()
         self.assertEqual(len(servers), 2)
@@ -26,14 +33,18 @@ class test_MdlServer(unittest.TestCase):
         self.assertTrue('test' in servers)
         
     def testCorrectServerNames(self):
-        """Test that the class associates the right server names with the selectors."""
+        '''
+        Test that the class associates the right server names with the selectors.
+        '''
         self.mdlsrv.parse()
         nodes = self.mdlsrv.servernodes
         self.assertEqual(nodes['prod']['name'], 'Production Moodle Server')
         self.assertEqual(nodes['test']['name'], 'Test Moodle Server')
         
     def testCorrectAreas(self):
-        """Test that the class returns the correct areas for a given server."""
+        '''
+        Test that the class returns the correct areas for a given server.
+        '''
         self.mdlsrv.parse()
         areas = self.mdlsrv.areanodes['prod'].keys()
         self.assertEqual(len(areas), 2)
@@ -41,7 +52,9 @@ class test_MdlServer(unittest.TestCase):
         self.assertTrue('stage' in areas)
     
     def testCorrectData(self):
-        """Test that the correct path data can be retrieved from the class."""
+        '''
+        Test that the correct path data can be retrieved from the class.
+        '''
         self.mdlsrv.parse()
         src = self.mdlsrv.datanodes['prod']['live']['src']
         data = self.mdlsrv.datanodes['test']['stage']['data']
